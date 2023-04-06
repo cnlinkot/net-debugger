b0c90dddea69e74eafb773428434e1d75eaf048d
= <=>
2023-04-06 21:29:49 +0800

    fix: hex截取错误

--- nd/encoder.go
+++ nd/encoder.go
@@ -31,8 +31,8 @@ func (h HexEncoder) Encode(bytes []byte) []byte {
 
 func (h HexEncoder) Decode(bytes []byte) []byte {
 	s := string(bytes)
-	s = strings.ReplaceAll(s, " ", "")[:len(s)-2]
-	b, err := hex.DecodeString(s)
+	s = strings.ReplaceAll(s, " ", "")
+	b, err := hex.DecodeString(s[:len(s)-2])
 	util.CheckError(err, "failed to decode hex")
 	return b
 }
